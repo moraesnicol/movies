1e4a43bdabc244b27adcb02ef26f7f56d3c0be69
Gabriel Nicol <moraesnicol@gmail.com>
2021-03-24 20:09:59 -0300

    movie_response

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -15,21 +15,42 @@ class MovieModel {
   final double voteAverage;
   final String overview;
   final DateTime releaseDate;
+
+  const MovieModel({
+    this.popularity,
+    this.voteCount,
+    this.video,
+    this.posterPath,
+    this.id,
+    this.adult,
+    this.backdropPath,
+    this.originalLanguage,
+    this.originalTitle,
+    this.genreIds,
+    this.title,
+    this.voteAverage,
+    this.overview,
+    this.releaseDate,
+  });
+
+  factory MovieModel.fromJson(String str) =>
+      MovieModel.fromMap(json.decode(str));
+
+  factory MovieModel.fromMap(Map<String, dynamic> json) => MovieModel(
+        popularity: json["popularity"].toDouble(),
+        voteCount: json["vote_count"],
+        video: json["video"],
+        posterPath: json["poster_path"],
+        id: json["id"],
+        adult: json["adult"],
+        backdropPath: json["backdrop_path"],
+        originalLanguage: json["original_language"],
+        originalTitle: json["original_title"],
+        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
+        title: json["title"],
+        voteAverage: json["vote_average"].toDouble(),
+        overview: json["overview"],
+        releaseDate: DateTime.parse(json["release_date"]),
+      );
 }
 
-const MovieModel({
-  this.popularity,
-  this.voteCount,
-  this.video,
-  this.posterPath,
-  this.id,
-  this.adult,
-  this.backdropPath,
-  this.originalLanguage,
-  this.originalTitle,
-  this.genreIds,
-  this.title,
-  this.voteAverage,
-  this.overview,
-  this.releaseDate,
-});
