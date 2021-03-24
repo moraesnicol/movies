eb5971a8606593c848d380bea98446e259e853fc
Gabriel Nicol <moraesnicol@gmail.com>
2021-03-24 20:20:47 -0300

    MovieGenre

--- lib/models/movie_detail_model.dart
+++ lib/models/movie_detail_model.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
-
 import 'movie_genre.dart';
 import 'production_company_model.dart';
 import 'production_country_model.dart';
@@ -11,6 +9,7 @@ class MovieDetailModel {
   final bool adult;
   final String backdropPath;
   final dynamic belongsToCollection;
+  final int budget;
   final List<MovieGenre> genres;
   final String homepage;
   final int id;
@@ -25,41 +24,77 @@ class MovieDetailModel {
   final DateTime releaseDate;
   final int revenue;
   final int runtime;
-  final List<SpokenLanguageModel> spokenLanguage;
+  final List<SpokenLanguageModel> spokenLanguages;
   final String status;
   final String tagline;
   final String title;
   final bool video;
   final double voteAverage;
   final int voteCount;
+
+  const MovieDetailModel({
+    this.adult,
+    this.backdropPath,
+    this.belongsToCollection,
+    this.budget,
+    this.genres,
+    this.homepage,
+    this.id,
+    this.imdbId,
+    this.originalLanguage,
+    this.originalTitle,
+    this.overview,
+    this.popularity,
+    this.posterPath,
+    this.productionCompanies,
+    this.productionCountries,
+    this.releaseDate,
+    this.revenue,
+    this.runtime,
+    this.spokenLanguages,
+    this.status,
+    this.tagline,
+    this.title,
+    this.video,
+    this.voteAverage,
+    this.voteCount,
+  });
+
+  factory MovieDetailModel.fromJson(String str) =>
+      MovieDetailModel.fromMap(json.decode(str));
+
+  factory MovieDetailModel.fromMap(Map<String, dynamic> json) =>
+      MovieDetailModel(
+        adult: json["adult"],
+        backdropPath: json["backdrop_path"],
+        belongsToCollection: json["belongs_to_collection"],
+        budget: json["budget"],
+        genres: List<MovieGenre>.from(
+            json["genres"].map((x) => MovieGenre.fromMap(x))),
+        homepage: json["homepage"],
+        id: json["id"],
+        imdbId: json["imdb_id"],
+        originalLanguage: json["original_language"],
+        originalTitle: json["original_title"],
+        overview: json["overview"],
+        popularity: json["popularity"].toDouble(),
+        posterPath: json["poster_path"],
+        productionCompanies: List<ProductionCompanyModel>.from(
+            json["production_companies"]
+                .map((x) => ProductionCompanyModel.fromMap(x))),
+        productionCountries: List<ProductionCountryModel>.from(
+            json["production_countries"]
+                .map((x) => ProductionCountryModel.fromMap(x))),
+        releaseDate: DateTime.parse(json["release_date"]),
+        revenue: json["revenue"],
+        runtime: json["runtime"],
+        spokenLanguages: List<SpokenLanguageModel>.from(json["spoken_languages"]
+            .map((x) => SpokenLanguageModel.fromMap(x))),
+        status: json["status"],
+        tagline: json["tagline"],
+        title: json["title"],
+        video: json["video"],
+        voteAverage: json["vote_average"].toDouble(),
+        voteCount: json["vote_count"],
+      );
 }
-
-const MovieDetailModel({
-  this.adult,
-  this.backdropPath,
-  this.belongsToCollection,
-  this.budget,
-  this.genres,
-  this.homepage,
-  this.id,
-  this.imdbId,
-  this.originalLanguage,
-  this.originalTitle,
-  this.overview,
-  this.popularity,
-  this.posterPath,
-  this.productionCompanies,
-  this.productionCountries,
-  this.releaseDate,
-  this.revenue,
-  this.runtime,
-  this.spokenLanguage,
-  this.status,
-  this.tagline,
-  this.title,
-  this.video,
-  this.voteAverage,
-  this.voteCount,
-});
-
-Factory
